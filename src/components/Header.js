cd4c0cba94b13b94f044915f72081d5e3a823ad3
Bayz.ng <108221204+Bayzng@users.noreply.github.com>
2023-10-20 16:11:35 -0700

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <>
+      <header className="flex items-center justify-between bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...  p-5 h-19">
+        <div className="logo">
+          <Link to="/">
+            <h2 className="font-bold text-xl md:text-2xl lg:text-3xl text-black">Elite Blog</h2>
+          </Link>
+        </div>
+
+        <nav>
+          <ul className="flex">
+            <li className="mr-5 font-bold text-xl md:text-2xl lg:text-3xl text-black">
+              <button>
+                <Link to="/">Home</Link>
+              </button>
+            </li>
+            <li className="font-bold text-xl md:text-2xl lg:text-3xl text-black">
+              <button>
+                <Link to="/blog">Blog</Link>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
